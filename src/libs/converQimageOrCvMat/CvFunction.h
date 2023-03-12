987cbbfbf444187c80cbf2e5b40136397c7c9b88
mrc <1552397900@qq.com>
2023-03-12 11:02:23 +0800

    2023.03.12.11.02.19

--- src/libs/converQimageOrCvMat/CvFunction.h
+++ src/libs/converQimageOrCvMat/CvFunction.h
@@ -3,17 +3,17 @@
 #pragma once
 
 namespace cvFunction {
-	inline void showMatImg( const std::string &name, const cv::Mat &showImg ) {
+	inline void showMatImg( const cv::Mat &showImg, const std::string &name ) {
 		cv::namedWindow(name, cv::WINDOW_NORMAL);
 		cv::imshow(name, showImg);
 	}
 
-	inline void showMatImg( const QString &name, const cv::Mat &showImg ) {
+	inline void showMatImg( const cv::Mat &showImg, const QString &name ) {
 		cv::imshow(name.toLocal8Bit().toStdString(), showImg);
 	}
 
 	inline void showMatImg( const cv::Mat &showImg ) {
-		cvFunction::showMatImg(std::to_string(cv::getTickCount()), showImg);
+		cvFunction::showMatImg(showImg, std::to_string(cv::getTickCount()));
 	}
 }
 
--- src/uis/demo001/main.cpp
+++ src/uis/demo001/main.cpp
@@ -1,4 +1,4 @@
-﻿#include <QCoreApplication>
+﻿#include <QGuiApplication>
 #include <opencv2/opencv.hpp>
 #include <opencv2/imgproc/types_c.h>
 #include <ConverQimageOrCvMat.h>
@@ -6,9 +6,11 @@
 #include <iostream>
 
 int main( int argc, char *argv[] ) {
-	QCoreApplication a(argc, argv);
+	QGuiApplication a(argc, argv);
 	cv::Mat readImage = cv::imread("waves.jpg");
-	std::cout << "rows : " << readImage.rows << ", " << "col : " << readImage.cols;
 	cvFunction::showMatImg(readImage);
+	std::string string = QString("行 : %1, 列 : %2").arg(readImage.rows).arg(readImage.cols).toLocal8Bit().toStdString();
+	std::cout << string << std::endl;
+	std::cout.flush();
 	return a.exec();
 }
